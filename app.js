e90fe53f10a50d22c369ac926aac9758ca66ae9c
toldi-richard <sze0tolric@vasvari.org>
2022-06-17 09:44:33 +0200

    kosár + gomb már a plussz terméket megkapja, de a kosár még nem frissül, függvénybe ki kell még szervezni

--- app.js
+++ app.js
@@ -64,6 +64,25 @@ products.forEach(product => {
 // Kosár kezelése ******************************************************
 const cart = {}
 
+const addToCart = (event) => {
+    // kosárba és a + gombok szétválasztása
+
+    // if else ágakat kerülni érdemes, főleg ha nagy a body-ja
+    // let target = event.target.dataset.id
+    // if (event.target.id) {
+    //    target = event.target.id
+    // }
+
+    // ternary operátor
+    let target = event.target.id ? event.target.id : event.target.dataset.id
+
+    // Ha még nincs a kosárban akkor hozzáadjuk 1 db-vel
+    if (cart[target] == undefined) {
+        cart[target] = 1
+        // Ha benne van akkor a darabszámát növeljük
+    } else {cart[target]++}
+}
+
 // Kigyűjtjük az addtocart css class-ú elemeket
 const addToCartButtons = document.getElementsByClassName('addToCart')
 // Megnézzük, hogy mennyi van belőle
@@ -71,16 +90,7 @@ const buttonCount = addToCartButtons.length
 
 for (let i = 0; i < buttonCount; i++) {
     // Klikk figyelő
-    addToCartButtons[i].addEventListener('click', function (event) {
-        // console.log(event.target.id)
-
-        // Ha még nincs a kosárban akkor hozzáadjuk 1 db-vel
-        if (cart[event.target.id] == undefined) {
-            cart[event.target.id] = 1
-
-            // Ha benne van akkor a darabszámát növeljük
-        } else {cart[event.target.id]++}
-    })
+    addToCartButtons[i].addEventListener('click',addToCart) // callback függvény, nem kell a () se az event átadás
 }
 
 const cartIcon = document.getElementById('cart-icon')
@@ -91,13 +101,15 @@ let total = 0
 cartIcon.addEventListener('click', function(event) {
     cartContent.classList.toggle('active')
     cartIcon.classList.toggle('active')
+
     cartItems.innerHTML = ''
+
     for (const id in cart) {
         const currentProduct = products.find(product => product.id == id)
 
         cartItems.innerHTML += 
             `<li>
-            <button data-id="${currentProduct.id}">+</button>
+                <button data-id="${currentProduct.id}"> + </button>
                 ${cart[id]} db -
                 ${currentProduct.name}
                 * ${currentProduct.price} Ft/db
@@ -110,8 +122,65 @@ cartIcon.addEventListener('click', function(event) {
         // console.log(cart[id])
         // console.log(products.find(product => product.id == id).price)
     }
+
     cartItems.innerHTML += `<li>
+
     Összesen: ${total} Ft
     </li>`
+
+    // eseménykezelőre eseménykezelőt berakni nem biztos hogy érdemes, nagyobb programoknál
 })
 
+// querySelector 1 element választ ki ami megfelel a paramétereknek
+const plusButtons = document.querySelectorAll('#car-items button')
+// plusButtons.addEventListener()
+// event delegation
+
+// document.getElementsByTagName('body')[0].addEventListener('click', () => console.log('katt'))
+// document.getElementsByTagName('main')[0].addEventListener('click', () => console.log('katt'))
+
+cartItems.addEventListener('click', addToCart)
+
+
+
+
+
+
+
+// Függvények ************************************************************************
+// js - blockscope,a változó csak az első záró zárójelig él, kivéve a globális változók
+
+// Nyíl függvény
+const addToCart1 = () => {
+}
+
+// fat arrow esetén a () elhagyható
+const addToCart2 = name => {
+    console.log(name)
+}
+
+// ha a függvény test csak egy utasításból áll, elhagyható a {} is
+const addToCart3 = name =>  console.log(name)
+
+// 2-nél több argumentum adása nem ajánlott, akkor már objektumot érdemes adni neki
+const person = {name: '', age: 0}
+const hello = (person) => {
+    console.log(person.name)
+}
+
+// visszatérési érték
+const multiplier = (num1, num2) => {
+    return num1 * num2
+}
+
+// Hagyományos függvény
+const oldStyle = function () {
+}
+
+const tesztelek = name => console.log('Szia '+ name)
+const tesztelek2 = name => {return console.log('Szia '+ name)}
+
+// return 1 sorosnál elhagyható, de 1 argumentumnál a () nem hagyható el
+const multiplier2 = (num1, num2) => num1*num2
+
+// Függvények ************************************************************************
